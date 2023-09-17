4f9627b41205e9d57b180cd327423ddaabe4801e
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2023-09-17 12:19:18 -0500

    Added matplotlib graphs of closing info

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 # Mini project 1
 
 import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def get_closing(ticker_list):
+def get_closing(ticker_list:list):
     # get 10 day history of each stock
     hist = [yf.Ticker(stock).history(period="10d") for stock in ticker_list]
 
@@ -17,5 +19,36 @@ def get_closing(ticker_list):
 
     return closing_list
 
+
+def get_closing_one(ticker):
+    # Get 10 day history of a stock
+    hist = yf.Ticker(ticker).history(period="10d")
+    # Get the closing price for each day
+    closing_list = [price for price in hist["Close"]]
+
+    return closing_list
+
+
 stocks = ["META", "AMZN", "GOOG", "NFLX", "MSFT"]
-print(get_closing(stocks))
+for stock in stocks:
+    # y-axis for our graph
+    stock_closing = np.array(get_closing_one(stock))
+    # x-axis for our graph
+    days = list(range(1, len(stock_closing) + 1))
+    # Plot the graph
+    plt.plot(days, stock_closing)
+
+    # get the min, max of our y-axis
+    prices = stock_closing
+    prices.sort()
+    low_price = prices[0]
+    high_price = prices[-1]
+    # set the x-axis & y-axis
+    plt.axis([1, 10, low_price - 1, high_price + 1])
+
+    # Labels for the graph
+    plt.title(f"Closing Price for {stock}")
+    plt.xlabel("Days")
+    plt.ylabel("Closing Price")
+    # render graph
+    plt.show()
